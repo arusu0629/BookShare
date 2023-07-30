1d163b1ffebbb4d8bf094a6567f2cd6bdd085d34
t-nakandakari <arusu0629@gmail.com>
2023-07-30 19:20:46 +0900

    セッション情報を使ってログインするように修正

--- post_instagram.py
+++ post_instagram.py
@@ -2,11 +2,21 @@ from instagrapi import Client
 from dotenv import load_dotenv
 import os
 
+# JSON ファイルの名前
+JSON_FILE = "session.json"
+
 def login_instagram(username, password):
-    print(username, password)
-    api = Client()
-    api.login(username, password)
-    return api
+    client = Client()
+    if os.path.exists(JSON_FILE):
+        print("Found session file.")
+        client.load_settings(JSON_FILE)
+        client.login(username, password)
+    else:
+        print("Session file not found.")
+        client.login(username, password)
+        client.dump_settings(JSON_FILE)
+
+    return client
 
 def post_to_instagram(api, description, hashtags, image_paths):
     caption = f"{description}\n{hashtags}"
